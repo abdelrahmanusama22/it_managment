ca47d4065495381220067e608076a5dd4448ca74
Abdelrahman Usama <142916119+abdelrahmanusama22@users.noreply.github.com>
2026-09-02 21:01:52 +0300

    Replace default import and export actions with custom ones

--- app/Filament/Resources/Devices/Pages/ListDevices.php
+++ app/Filament/Resources/Devices/Pages/ListDevices.php
@@ -3,7 +3,11 @@
 namespace App\Filament\Resources\Devices\Pages;
 
 use App\Filament\Resources\Devices\DeviceResource;
+use App\Filament\Actions\CustomImportAction;
+use App\Filament\Imports\DeviceImporter;
 use Filament\Actions\CreateAction;
+use Filament\Actions\ExportAction;
+use App\Filament\Exports\DeviceExporter;
 use Filament\Resources\Pages\ListRecords;
 
 class ListDevices extends ListRecords
@@ -13,10 +17,10 @@ class ListDevices extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            \Filament\Actions\ImportAction::make()
-                ->importer(\App\Filament\Imports\DeviceImporter::class),
-            \Filament\Actions\ExportAction::make()
-                ->exporter(\App\Filament\Exports\DeviceExporter::class),
+            CustomImportAction::make()
+                ->importer(DeviceImporter::class),
+            ExportAction::make()
+                ->exporter(DeviceExporter::class),
             CreateAction::make(),
         ];
     }
